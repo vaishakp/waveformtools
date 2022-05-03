d0f684ef67943892218c01b630a30fc765b28b9a
Vaishak Prasad <vaishakprasad@gmail.com>
2022-05-03 11:46:05 +0000

    Update docs/source/conf_old.py, docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,19 +12,25 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
-sys.setrecursionlimit(1500)
-import sphinx_rtd_theme
+
+import sphinx
+
+from recommonmark.parser import CommonMarkParser
+
+# -- Project information -----------------------------------------------------
+
+from waveformtools import __version__ as waveformtools_version
 
 # -- Project information -----------------------------------------------------
 
 project = 'waveformtools'
-copyright = '2020, Vaishak P'
-author = 'P. Vaishak '
+copyright = '2020, Vaishak Prasad'
+author = 'Vaishak Prasad'
 
+# The short X.Y version
+version = waveformtools_version
 # The full version, including alpha/beta/rc tags
-release = '2020'
-
+release = version
 
 # -- General configuration ---------------------------------------------------
 
@@ -32,38 +38,88 @@ release = '2020'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'rinoh.frontend.sphinx',
-        'sphinx.ext.autodoc',
-        "sphinx_rtd_theme"
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "numpydoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "recommonmark",
 ]
 
+autosummary_generate = True
+
+autodoc_docstring_signature = True
+if sphinx.version_info < (1, 8):
+    autodoc_default_flags = ["members", "undoc-members"]
+else:
+    autodoc_default_options = {"members": None, "undoc-members": None, "special-members": "__call__"}
+
+# -- Try to auto-generate numba-decorated signatures -----------------
+
+import numba
+import inspect
+
+
+def process_numba_docstring(app, what, name, obj, options, signature, return_annotation):
+    if type(obj) is not numba.core.registry.CPUDispatcher:
+        return (signature, return_annotation)
+    else:
+        original = obj.py_func
+        orig_sig = inspect.signature(original)
+
+        if (orig_sig.return_annotation) is inspect._empty:
+            ret_ann = None
+        else:
+            ret_ann = orig_sig.return_annotation.__name__
+
+        return (str(orig_sig), ret_ann)
+
+
+def setup(app):
+    app.connect("autodoc-process-signature", process_numba_docstring)
+
+
+# --------------------------------------------------------------------
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+if sphinx.version_info < (1, 8):
+    source_parsers = {
+        ".md": CommonMarkParser,
+    }
+    source_suffix = [".rst", ".md"]
+else:
+    source_suffix = {
+        ".rst": "restructuredtext",
+        ".txt": "markdown",
+        ".md": "markdown",
+    }
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-
-#html_theme = 'alabaster'
+#
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-# -- Options for LaTeX output ------------------------------------------------
+# -- Options for todo extension ----------------------------------------------
 
-latex_elements = {
-        'papersize': 'letterpaper', 
-        'pointsize': '10pt',
-        'preamble': '',
-        'figure_align': 'htbp'
-}
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
