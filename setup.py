acfb48defa8a22c07043d74af012c9bb13bcd884
vaishakp <vaishakprasad@gmail.com>
2023-07-14 21:29:00 +0530

    new release

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_requirements():
 
 setuptools.setup(
     name="waveformtools",
- 	version="2023.06.27",
+ 	version="2023.07.14",
     author="Vaishak Prasad",
     author_email="vaishakprasad@gmail.com",
     description="Functions for handling waveform and numerical relativity data",
--- waveformtools/__init__.py
+++ waveformtools/__init__.py
@@ -40,4 +40,4 @@ def read_git_version():
     return vers
 
 
-__version__ = "2023.06.27"
+__version__ = "2023.07.14"
