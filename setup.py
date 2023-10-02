afeb36ca2f003306f6cd581398173e58c369c507
Vaishak Prasad <vaishakprasad@gmail.com>
2023-10-02 11:38:49 +0530

    allow spherical tests

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_requirements():
 
 setuptools.setup(
     name="waveformtools",
- 	version="2023.09.28",
+ 	version="2023.10.02",
     author="Vaishak Prasad",
     author_email="vaishakprasad@gmail.com",
     description="Functions for handling waveform and numerical relativity data",
--- waveformtools/__init__.py
+++ waveformtools/__init__.py
@@ -40,4 +40,4 @@ def read_git_version():
     return vers
 
 
-__version__ = "2023.09.28"
+__version__ = "2023.10.02"
