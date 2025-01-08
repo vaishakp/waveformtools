57ca912b6bd4969ecd64106e4138bab69e40be60
Vaishak Prasad <vaishakprasad@gmail.com>
2025-01-08 21:56:21 +0530

    Lint

--- dev/notebooks/extrapolate_icts.py
+++ dev/notebooks/extrapolate_icts.py
@@ -5,31 +5,34 @@ import config
 import subprocess
 
 import matplotlib.pyplot as plt
+
 config.conf_matplolib()
 
 
 from waveformtools.sxs.discover_sims import SimulationExplorer
 
-list_of_search_dirs = ['/mnt/pfs/vaishak.p/sims/SpEC/gcc/bfi/ICTSEccParallel']
+list_of_search_dirs = ["/mnt/pfs/vaishak.p/sims/SpEC/gcc/bfi/ICTSEccParallel"]
 
-se = SimulationExplorer(search_dir=list_of_search_dirs[0], prepared_waveforms_dir=Path('/mnt/pfs/vaishak.p/Projects/Codes/waveforms'))
+se = SimulationExplorer(
+    search_dir=list_of_search_dirs[0],
+    prepared_waveforms_dir=Path("/mnt/pfs/vaishak.p/Projects/Codes/waveforms"),
+)
 
 se.search_simulations()
 se.find_highest_ecc_dirs()
 se.discover_levels()
 
-#se.prepare_waveforms()
+# se.prepare_waveforms()
 
 
 for sim_name, sim_path in se.found_sim_paths.items():
 
-    inside_ev = sim_path.joinpath(f'Ecc{se.highest_ecc_nums[sim_name]}/Ev')
+    inside_ev = sim_path.joinpath(f"Ecc{se.highest_ecc_nums[sim_name]}/Ev")
 
-    out_file = sim_path.joinpath('WaveformsJoinedLev3.h5')
+    out_file = sim_path.joinpath("WaveformsJoinedLev3.h5")
 
-    cmd = f'JoinH5 -v -o {out_file} -l {inside_ev}/Lev3*/Run/GW2/rh_FiniteRadii_CodeUnits.h5'
+    cmd = f"JoinH5 -v -o {out_file} -l {inside_ev}/Lev3*/Run/GW2/rh_FiniteRadii_CodeUnits.h5"
 
     print(cmd)
 
     (cmd)
-
