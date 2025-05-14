cc15827ff510bf4dc7d5f99803c4e5378774f60a
Vaishak Prasad <vaishakprasad@gmail.com>
2025-05-14 16:29:09 -0400

    bugfix versioning

--- .generate_version.py
+++ .generate_version.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import os
-from datetime import date
+import datetime
+#from datetime import date
 import re
 source_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -43,8 +44,9 @@ def write_git_version():
     # Open the file
     # Get the version
     # vers = os.popen(f'git -C {source_directory} log -1 --date=short | grep Date').read()[8:-1]
-    new_major_version = str(date.today())
-    new_major_version = new_version.replace("-", ".")
+    # new_major_version = str(date.today())
+    new_major_version = str(datetime.datetime.now(datetime.UTC).date())
+    new_major_version = new_major_version.replace("-", ".")
 
     current_version, current_major_version, current_minor_version = get_current_version()
     
@@ -62,34 +64,34 @@ def write_git_version():
 
     # Write to public/version
     with open(source_directory + "/public/version", "w") as vers_file:
-        vers_file.writelines([vers])
+        vers_file.writelines([new_version])
 
     # write to __init__.py
     ee = os.popen(
-        f"sed -i '/__version__/c\\__version__ = \"{vers}\"' {source_directory}/waveformtools/__init__.py"
+        f"sed -i '/__version__/c\\__version__ = \"{new_version}\"' {source_directory}/waveformtools/__init__.py"
     )
     # with open(package_directory+'/waveformtools/__init__.py', 'r') as init_file:
     # with open(package_directory + "/../public/date.txt", "r") as vers_file:
     # vers = vers_file.read()[:10]
 
     ee = os.popen(
-        f"sed -i '/version/c\\ \tversion=\"{vers}\",' {source_directory}/setup.py"
+        f"sed -i '/version/c\\ \tversion=\"{new_version}\",' {source_directory}/setup.py"
     )
-    print("Version", vers)
+    print("Version", new_version)
 
     # Generate badge
     badge = f"""<svg width="140" height="20" xmlns="http://www.w3.org/2000/svg">
                 <rect width="80" height="20" rx="0" ry="5" fill="grey" x="0" y="0" />
                 <rect width="60" height="20" fill="blue" x="80" y="0" />
                 <text x="6" y="13" fill="white" font-size="11">pypi package</text>
-                <text x="83" y="14" fill="white" font-size="11">{vers}</text>
+                <text x="83" y="14" fill="white" font-size="11">{new_version}</text>
                 </svg>
             """
 
     with open("docs/vers_badge.svg", "w") as svgf:
         svgf.write(badge)
 
-    return vers
+    return new_version
 
 
 write_git_version()
