c35e811a23296ff4a6d5cccf9b7e90e4926b1124
Vaishak Prasad <vaishakprasad@gmail.com>
2025-06-17 09:46:24 -0400

    add egg name

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_requirements():
 
 setuptools.setup(
     name="waveformtools",
- 	version="2025.06.17",
+ 	version="2025.06.17.1",
     author="Vaishak Prasad",
     author_email="vaishakprasad@gmail.com",
     description="Functions for handling waveform and numerical relativity data",
