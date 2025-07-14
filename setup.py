ee4546577dc60acef1a6e89d384814706455a81d
Vaishak Prasad <vaishakprasad@gmail.com>
2025-07-14 14:43:38 -0400

    release

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_requirements():
 
 setuptools.setup(
     name="waveformtools",
- 	version="2025.07.14",
+ 	version="2025.07.14.1",
     author="Vaishak Prasad",
     author_email="vaishakprasad@gmail.com",
     description="Functions for handling waveform and numerical relativity data",
