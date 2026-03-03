11838500099589497e51217ffd18ea7918371a0b
Vaishak Prasad <vaishakprasad@gmail.com>
2026-03-03 11:06:04 -0500

    add scipy vers

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_requirements():
 
 setuptools.setup(
     name="waveformtools",
- 	version="2026.03.03",
+ 	version="2026.03.03.1",
     author="Vaishak Prasad",
     author_email="vaishakprasad@gmail.com",
     description="Functions for handling waveform and numerical relativity data",
--- waveformtools/__init__.py
+++ waveformtools/__init__.py
@@ -44,4 +44,4 @@ def read_git_version():
     return vers
 
 
-__version__ = "2026.03.03"
+__version__ = "2026.03.03.1"
