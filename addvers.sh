e02e9998fdd9c6122697300e8d5784473de4e9f6
Vaishak Prasad <36019754+vaishakp@users.noreply.github.com>
2026-06-02 08:45:26 -0400

    Make version bump script deterministic

--- addvers.sh
+++ addvers.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-#echo $PWD
-CWD=$(basename $(pwd))
-#echo ${CWD}
-/usr/bin/env python .generate_version.py
-git add ${CWD}/__init__.py
-git add ${PWD}/setup.py
-git add ${PWD}/public/version
-git add ${PWD}/docs/vers_badge.svg
+python .generate_version.py
+
+git add waveformtools/__init__.py
+git add setup.py
+git add public/version
+git add docs/vers_badge.svg
